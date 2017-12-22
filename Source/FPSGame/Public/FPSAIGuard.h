9eb9c0fd2e73dd2939c25cf0cb1ec6a85051cd57
geostik <cgiteach@gmail.com>
2017-12-22 11:00:34 +0200

    Setup Sight Sense

--- Source/FPSGame/Public/FPSAIGuard.h
+++ Source/FPSGame/Public/FPSAIGuard.h
@@ -24,6 +24,8 @@ protected:
 	UPROPERTY(VisibleAnywhere, Category = "Components")
 	UPawnSensingComponent* PawnSensingComp;
 
+	UFUNCTION()
+	void OnPawnSeen(APawn* SeenPawn);
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
