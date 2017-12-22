e2db545fc110d046b67934ea95d194d732e23456
geostik <cgiteach@gmail.com>
2017-12-22 11:20:32 +0200

    Setup hearing sense

--- Source/FPSGame/Public/FPSAIGuard.h
+++ Source/FPSGame/Public/FPSAIGuard.h
@@ -26,6 +26,8 @@ protected:
 
 	UFUNCTION()
 	void OnPawnSeen(APawn* SeenPawn);
+	UFUNCTION()
+		void OnNoiseHeard(APawn* NoiseInstigator, const FVector& Location, float Volume);
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
